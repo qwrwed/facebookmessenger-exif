b007aad373eb49fd9365289d78ba2620e8061d70
qwrwed <qwrwed@users.noreply.github.com>
2022-10-03 09:18:49 +0100

    rename thumbnails to match corresponding videos

--- thumbs.py
+++ thumbs.py
@@ -1,13 +1,15 @@
 import argparse
+import math
 import os
 from pathlib import Path
+import shutil
 
 import cv2
 import numpy as np
 import exiftool
 from tqdm import tqdm
 
-DIFFERENCE_THRESHOLD = 0.01
+DIFFERENCE_THRESHOLD = 0.05
 
 
 def _normpath(path):
@@ -18,6 +20,7 @@ class ProgramArgsNamespace(argparse.Namespace):
     backup: bool
     input_dir: Path
     show_images_unsafe: bool
+    rename: bool
 
 
 def get_args():
@@ -46,6 +49,12 @@ def get_args():
         action="store_true",
         help="Backup modified files",
     )
+    parser.add_argument(
+        "-r",
+        "--rename",
+        action="store_true",
+        help="Rename matched thumbnails to corresponding videos",
+    )
     return parser.parse_args(namespace=ProgramArgsNamespace)
 
 
@@ -64,25 +73,34 @@ if __name__ == "__main__":
     pattern = "*.jpg" if input_dir.parts[-1] == "thumbnails" else "**/thumbnails/*.jpg"
     thumbnail_paths = list(args.input_dir.glob(pattern))
 
-    already_mapped_videos = set()
+    mapped_videos: set[Path] = set()
+    seen_videos: set[Path] = set()
+    # TODO: investigate the case where videos and thumbnails are not 1:1 - one is duplicated, the other isn't
 
     exiftool_params_write = []
     if not args.backup:
         exiftool_params_write.append("-overwrite_original")
 
     with exiftool.ExifToolHelper() as et:
-        for thumbnail_path in tqdm(thumbnail_paths):
-
+        pbar = tqdm(thumbnail_paths)
+        for thumbnail_path in pbar:
+            thumbnail_path_relative = Path(
+                *thumbnail_path.parts[len(input_dir.parts) :]
+            )
+            pbar.set_description(str(thumbnail_path_relative))
             mapped = False
             thumbnail_image: "np.ndarray" = cv2.imread(str(thumbnail_path))
 
             thumbnail_dir = thumbnail_path.parent
             video_dir = thumbnail_path.parent.parent
-
             video_paths = video_dir.glob("*.mp4")
+
+            max_similarity = 0
+            most_similar_video_name = "<None>"
             for video_path in video_paths:
-                if video_path in already_mapped_videos:
+                if video_path in mapped_videos:
                     continue
+                seen_videos.add(video_path)
 
                 vidcap = cv2.VideoCapture(str(video_path))
                 success: bool
@@ -95,12 +113,12 @@ if __name__ == "__main__":
                     firstframe_image, thumbnail_image.shape[:2][::-1]
                 )
 
-                difference = (
-                    1
-                    - cv2.matchTemplate(
-                        thumbnail_image, firstframe_image, cv2.TM_CCOEFF_NORMED
-                    )[0][0]
-                )
+                similarity = cv2.matchTemplate(
+                    thumbnail_image, firstframe_image, cv2.TM_CCOEFF_NORMED
+                )[0][0]
+                if similarity > max_similarity:
+                    max_similarity = similarity
+                    most_similar_video_name = video_path.name
 
                 if args.show_images_unsafe:
                     thumbnail_window_title = f"reference | thumb {thumbnail_path}"
@@ -112,11 +130,12 @@ if __name__ == "__main__":
                     cv2.imshow(thumbnail_window_title, thumbnail_image)
                     cv2.imshow(video_window_title, firstframe_image)
 
+                difference = 1 - similarity
                 if difference < DIFFERENCE_THRESHOLD:
                     # tqdm.write(
                     #     f"{Path(*thumbnail_path.parts[-4:])} => {Path(*video_path.parts[-2:])}"
                     # )
-                    already_mapped_videos.add(video_path)
+                    mapped_videos.add(video_path)
                     mapped = True
                     if args.show_images_unsafe:
                         cv2.imshow(video_window_title, firstframe_image)
@@ -133,7 +152,7 @@ if __name__ == "__main__":
                 video_date_metadata = {
                     tag: value
                     for tag, value in video_metadata.items()
-                    if tag in thumbnail_metadata and tag.endswith("Date")
+                    if tag in thumbnail_metadata and "Date" in tag
                 }
                 date = video_date_metadata["File:FileCreateDate"]
                 video_date_metadata.update(
@@ -147,7 +166,25 @@ if __name__ == "__main__":
                     tags=video_date_metadata,
                     params=exiftool_params_write,
                 )
-                thumbnail_metadata_new = et.get_metadata(thumbnail_path)[0]
+                # thumbnail_metadata_new = et.get_metadata(thumbnail_path)[0]
+                if args.rename:
+                    thumbnail_path.rename(
+                        Path(
+                            thumbnail_path.parent,
+                            f"{video_path.stem}_thumb{thumbnail_path.suffix}",
+                        )
+                    )
             else:
                 # tqdm.write(f"{Path(*thumbnail_path.parts[-4:])} <= {None}")
+                tqdm.write(
+                    f"Could not find video for '{thumbnail_path_relative}' (max similarity={(max_similarity)*100:.0f}% in {most_similar_video_name})"
+                )
                 pass
+
+    # create_folder_with_copies_of_unmapped_videos = False
+    # if create_folder_with_copies_of_unmapped_videos:
+    #     unmapped_videos = seen_videos - mapped_videos
+    #     for video_path in seen_videos - mapped_videos:
+    #         unmapped_dir = Path(video_path.parent, "unmapped")
+    #         unmapped_dir.mkdir(exist_ok=True)
+    #         shutil.copy(video_path, Path(unmapped_dir, video_path.name))
